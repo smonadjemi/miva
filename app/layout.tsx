9f7409c6e077deedf2e9c3ea51e0886a2e371e2d
Shayan Monadjemi <shayan.monadjemi@gmail.com>
2025-11-19 13:13:33 -0500

    add taxonomy desciption

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,17 @@ import "./globals.css";
 import { ColorSchemeScript, MantineProvider, mantineHtmlProps } from '@mantine/core';
 import { Navbar } from "@/components/Navbar/Navbar";
 
+const blackScale: readonly [
+  string, string, string, string, string,
+  string, string, string, string, string
+] = [
+    '#ffffff', '#f2f2f2', '#e6e6e6', '#cccccc', '#999999',
+    '#666666', '#333333', '#1f1f1f', '#0f0f0f', '#000000'
+  ];
+
+
+
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -38,7 +49,15 @@ export default function RootLayout({
         <ColorSchemeScript forceColorScheme="light" defaultColorScheme="light" />
       </head>
       <body>
-        <MantineProvider forceColorScheme="light" defaultColorScheme="light">
+        <MantineProvider
+          forceColorScheme="light"
+          defaultColorScheme="light"
+          theme={{
+            colors: { black: blackScale },
+            primaryColor: 'black',
+            primaryShade: 7
+          }}
+        >
           <Navbar />
           {children}
         </MantineProvider>
